e8686f418c22007ad0d24d28953ded71792575fc
Vikash Kumar <rajvikash18113@gmail.com>
2024-08-27 17:50:24 +0530

    Add files via upload

---
+++ Lecture 3/le3p8.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+    int a = 9;
+    if (a == 9){
+        cout << "NINE" << endl;
+    }
+
+    if(a > 0){
+        cout << "POSITIVE" << endl;
+
+    }
+    else{
+        cout << "NEGATIVE" << endl;
+    }
+} 
