f2fac4bb0effae1158083c757b3011aec128832a
Vikash Kumar <rajvikash18113@gmail.com>
2024-09-05 16:39:52 +0530

    Create le4p10.cpp

---
+++ Lecture 4/le4p10.cpp
@@ -0,0 +1,60 @@
+/*
+ABCD
+ABCD
+ABCD
+ABCD
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int row = 1;
+
+    while( row<=n)
+    {
+        int col = 1;
+        char ch = 'A';
+        while (col <= n)
+        {
+            cout << ch;
+            ch = ch + 1;
+            col = col + 1;
+
+        }
+        cout << endl;
+        row = row + 1;
+    }
+}
+
+                        // Method 2nd
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     int row = 1;
+
+//     while (row <= n)
+//     {
+//         int col = 1;
+
+//         while (col <= n)
+//         {
+//             char ch = 'A' + col - 1;
+//             cout << ch;
+//             col = col + 1;
+//         }
+//         cout << endl;
+//         row = row + 1;
+//     }
+// }
